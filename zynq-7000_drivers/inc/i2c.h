63ee1dcfcef391e1023ac12ae5ca5e27ee4b429d
drakonof <soikadigital@gmail.com>
2021-06-03 17:55:02 +0300

    030621

--- zynq-7000_drivers/inc/i2c.h
+++ zynq-7000_drivers/inc/i2c.h
@@ -1,22 +1,42 @@
 #ifndef INC_I2C_H
 #define INC_I2C_H
 
-#include "types.h"
+#include "types_.h"
 
-typedef struct
-{
-	uint32_t sclk_rate;
+typedef struct {
+	uint32_t         id;
+	boolean          do_unblocking_mode;
+//	void (*interrupt_handler) (void *, uint32_t);
+
+	volatile status  init;
+
+	uint32_t         sclk_rate;
 } i2c_inition;
 
-typedef struct
-{
-	volatile boolean ready;
-//	boolean          do_unblocking_mode;
+typedef struct {
 	uint32_t         id;
-	uint8_t          i2c_address;
-	volatile status  init;
+	boolean          do_unblocking_mode;
+
+	volatile boolean is_ready;
+
+	boolean          do_master; //-
+
+	uint8_t          bus_address;
+	boolean          do_rep_start; //-
+    boolean          do_ten_bit_address;
+
+    uint8_t          *tx_buffer;
+    uint8_t          *rx_buffer;
+
+    size_t           size;
 } i2c_handle;
 
-status i2c_init(i2c_handle *p_handle, i2c_inition *init);
+status i2c_init(i2c_inition *p_init);
+status i2c_reinit(i2c_inition *p_init);
+status i2c_release(i2c_handle *p_handle); //todo
+status i2c_write(i2c_handle *p_handle);
+status i2c_read(i2c_handle *p_handle);
+boolean i2c_get_ready(); //todo
+status i2c_reset(i2c_handle *p_handle); //todo
 
 #endif /* INC_I2C_H */
--- zynq-7000_drivers/src/i2c.c
+++ zynq-7000_drivers/src/i2c.c
@@ -1,91 +1,220 @@
+#include "xparameters.h"
+#include "xscugic.h"
+#include "xil_exception.h"
+#include "xiicps.h"
+
 #include "i2c.h"
 
-#include "xparameters.h"
-#include "xiicps.h"
-#include "xil_printf.h"
+XScuGic InterruptController;	/* Instance of the Interrupt Controller */
 
-#define IIC_DEVICE_ID		XPAR_XIICPS_0_DEVICE_ID
+static status interrupt_init_(uint32_t id);
+static status write_block_mode_(uint32_t id, uint8_t *p_data, size_t size, uint8_t address);
+static status write_un_block_mode_(uint32_t id, uint8_t *p_data, size_t size, uint8_t address);
 
-#define IIC_SLAVE_ADDR		0x68
-#define IIC_SCLK_RATE		100000
-#define TEST_BUFFER_SIZE	8
+void interrupt_handler_(void *CallBackRef, u32 Event); // STATIC?
 
-int IicPsMasterPolledExample(u16 DeviceId);
-XIicPs Iic;
+static XIicPs Iic[XPAR_XIICPS_NUM_INSTANCES];
 
-u8 SendBuffer[TEST_BUFFER_SIZE];
-u8 RecvBuffer[TEST_BUFFER_SIZE];
+status i2c_init(i2c_inition *p_init) {
+	XIicPs_Config *cfg = NULL;
 
-status i2c_init(i2c_handle *p_handle, i2c_inition *init) {
-	int Status;
-
-	Status = IicPsMasterPolledExample(p_handle->id);
-	if (Status != XST_SUCCESS) {
-		xil_printf("IIC Master Polled Example Test Failed\r\n");
-		return XST_FAILURE;
+	if (NULL == p_init) {
+		return error_;
 	}
 
-		xil_printf("Successfully ran IIC Master Polled Example Test\r\n");
-	return XST_SUCCESS;
+	p_init->init = un_init_;
+
+	if (NULL == (cfg = XIicPs_LookupConfig(p_init->id))) {
+		return error_;
+	}
+
+	if (XST_SUCCESS != XIicPs_CfgInitialize(&Iic[p_init->id],
+			                                cfg,
+											cfg->BaseAddress)) {
+		return error_;
+	}
+
+	if (true_ == p_init->do_unblocking_mode) {
+		if (ok_ != interrupt_init_(p_init->id)) {
+			return XST_FAILURE;
+		}
+
+		XIicPs_SetStatusHandler(&Iic[p_init->id], (void *) &Iic[p_init->id], interrupt_handler_);
+	}
+
+	if (XST_SUCCESS != XIicPs_SetSClk(&Iic[p_init->id],
+			                          p_init->sclk_rate)) {
+		return error_;
+	}
+
+	p_init->init = ok_;
+
+	return ok_;
 }
 
-int IicPsMasterPolledExample(u16 DeviceId)
+status i2c_reinit(i2c_inition *p_init) {
+	if (NULL == p_init) {
+		return error_;
+	}
+
+	if (ok_ != p_init->init) {
+		return p_init->init;
+	}
+
+	if (true_ == p_init->do_unblocking_mode) {
+		if (ok_ != interrupt_init_(p_init->id)) {
+			return XST_FAILURE;
+		}
+	}
+
+	if (XST_SUCCESS != XIicPs_SetSClk(&Iic[p_init->id],
+			                          p_init->sclk_rate)) {
+		return error_;
+	}
+
+    return ok_;
+}
+
+//todo:
+status i2c_release(i2c_handle *p_handle) {
+	return ok_;
+}
+
+status i2c_write(i2c_handle *p_handle) {
+
+
+	if (NULL == p_handle) {
+		return error_;
+	}
+
+	if (true_ == p_handle->do_ten_bit_address) {
+		if (XST_SUCCESS != XIicPs_SetOptions(&Iic[p_handle->id],
+				                             XIICPS_10_BIT_ADDR_OPTION)) {
+			return error_;
+		}
+	}
+	else {
+		if (XST_SUCCESS != XIicPs_ClearOptions(&Iic[p_handle->id],
+				                               XIICPS_10_BIT_ADDR_OPTION)) {
+			return error_;
+		}
+	}
+
+	if (true_ == p_handle->do_unblocking_mode) {
+		if (ok_ != write_un_block_mode_(p_handle->id,
+												  p_handle->tx_buffer,
+												  p_handle->size,
+												  p_handle->bus_address)) {
+					return error_;
+
+				}
+	}
+	else {
+		if (ok_ != write_block_mode_(p_handle->id,
+										  p_handle->tx_buffer,
+										  p_handle->size,
+										  p_handle->bus_address)) {
+			return error_;
+
+		}
+	}
+
+	return ok_;
+}
+
+static status write_block_mode_(uint32_t id, uint8_t *p_data, size_t size, uint8_t address) {
+	if (XST_SUCCESS != XIicPs_MasterSendPolled(&Iic[id], p_data, size, address)) {
+		return error_;
+	}
+
+	while (XIicPs_BusIsBusy(&Iic[id])) {
+		asm("NOP");
+	}
+
+	return ok_;
+}
+
+static status write_un_block_mode_(uint32_t id, uint8_t *p_data, size_t size, uint8_t address) {
+
+	XIicPs_MasterSend(&Iic[id], p_data, size,
+			address);
+
+	return ok_;
+}
+
+status i2c_read(i2c_handle *p_handle) {
+	if (NULL == p_handle) {
+		return error_;
+	}
+
+	if (true_ == p_handle->do_ten_bit_address) {
+		if (XST_SUCCESS != XIicPs_SetOptions(&Iic[p_handle->id],
+				                             XIICPS_10_BIT_ADDR_OPTION)) {
+			return error_;
+		}
+	}
+	else {
+		if (XST_SUCCESS != XIicPs_ClearOptions(&Iic[p_handle->id],
+				                               XIICPS_10_BIT_ADDR_OPTION)) {
+			return error_;
+		}
+	}
+
+	if (XST_SUCCESS != XIicPs_MasterRecvPolled(&Iic[p_handle->id],
+			                                   p_handle->rx_buffer,
+											   p_handle->size,
+											   p_handle->bus_address)) {
+		return error_;
+	}
+
+   return ok_;
+}
+
+//todo:
+status i2c_reset(i2c_handle *p_handle) {
+	return ok_;
+}
+
+static status interrupt_init_(uint32_t id) {
+	XScuGic_Config *interrupt_cfg = NULL;
+
+	Xil_ExceptionInit();
+
+	if (NULL == (interrupt_cfg = XScuGic_LookupConfig(XPAR_XIICPS_0_INTR))) {
+		return error_;
+	}
+
+	if (XST_SUCCESS != XScuGic_CfgInitialize(&InterruptController,
+			                                 interrupt_cfg,
+											 interrupt_cfg->CpuBaseAddress)) {
+		return error_;
+	}
+
+	Xil_ExceptionRegisterHandler(XIL_EXCEPTION_ID_IRQ_INT,
+			                    (Xil_ExceptionHandler) XScuGic_InterruptHandler,
+				                &InterruptController);
+
+	if (XST_SUCCESS != XScuGic_Connect(&InterruptController,
+			                           XIL_EXCEPTION_ID_IRQ_INT,
+			                           (Xil_InterruptHandler)XIicPs_MasterInterruptHandler,
+			                           (void *)&Iic[id])) {
+		return error_;
+	}
+
+	XScuGic_Enable(&InterruptController, XIL_EXCEPTION_ID_IRQ_INT);
+
+	Xil_ExceptionEnable();
+
+	return ok_;
+}
+
+void interrupt_handler_(void *call_back, uint32_t event)
 {
-	int Status;
-	XIicPs_Config *Config;
-	int Index;
-
-	Config = XIicPs_LookupConfig(DeviceId);
-	if (NULL == Config) {
-		return XST_FAILURE;
+	if (0 != (event & XIICPS_EVENT_COMPLETE_RECV)){
+	//	tx_complete = TRUE;
+	} else if (0 != (event & XIICPS_EVENT_COMPLETE_SEND)) {
+	//	rx_complete = TRUE;
+	} else if (0 == (event & XIICPS_EVENT_SLAVE_RDY)){
+	//	error_count++;
 	}
-
-	Status = XIicPs_CfgInitialize(&Iic, Config, Config->BaseAddress);
-	if (Status != XST_SUCCESS) {
-		return XST_FAILURE;
-	}
-
-	Status = XIicPs_SelfTest(&Iic);
-	if (Status != XST_SUCCESS) {
-		return XST_FAILURE;
-	}
-
-	XIicPs_SetSClk(&Iic, IIC_SCLK_RATE);
-
-	for (Index = 0; Index < TEST_BUFFER_SIZE; Index++) {
-		SendBuffer[Index] = (Index % TEST_BUFFER_SIZE);
-		RecvBuffer[Index] = 0;
-	}
-
-	//SendBuffer[1] = (1<<7);
-
-	Status = XIicPs_MasterSendPolled(&Iic, SendBuffer,
-			 TEST_BUFFER_SIZE, IIC_SLAVE_ADDR);
-	if (Status != XST_SUCCESS) {
-		return XST_FAILURE;
-	}
-
-	while (XIicPs_BusIsBusy(&Iic)) {
-
-	}
-
-	SendBuffer[0] = 0;
-
-	while (1) {
-
-		Status = XIicPs_MasterSendPolled(&Iic, SendBuffer,
-				 1, IIC_SLAVE_ADDR);
-		if (Status != XST_SUCCESS) {
-			return XST_FAILURE;
-		}
-
-		Status = XIicPs_MasterRecvPolled(&Iic, RecvBuffer,
-				  TEST_BUFFER_SIZE, IIC_SLAVE_ADDR);
- 		if (Status != XST_SUCCESS) {
-			return XST_FAILURE;
-		}
-
-	}
-
-	return XST_SUCCESS;
 }
