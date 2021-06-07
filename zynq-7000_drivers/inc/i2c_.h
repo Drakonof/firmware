f4977824808ce6b1a862fd0bec706f905eb6b13f
drakonof <soikadigital@gmail.com>
2021-06-07 10:54:13 +0300

    070621

--- zynq-7000_drivers/inc/i2c_.h
+++ zynq-7000_drivers/inc/i2c_.h
@@ -8,28 +8,31 @@
 
 #include "types_.h"
 
-typedef struct {
-	uint32_t id;
-	boolean do_unblocking_mode;
-	status init;
+typedef enum {
+    tx_ready_flag,
+    rx_ready_flag
+} i2c_ready_flags;
 
-    boolean do_master;
-    uint16_t self_address; // for slave mode
-	uint32_t sclk_rate;
+typedef struct {
+    boolean do_unblocking_mode;
     boolean do_ten_bit_address;
+    boolean do_master;
+
+    uint32_t id;
+    uint16_t self_address; // for slave mode
+    uint32_t sclk_rate;
+
+    status init;
 } i2c_inition;
 
 typedef struct {
-	uint32_t id;
 	boolean do_unblocking_mode;
-
+    boolean do_rep_start; //-
 	uint16_t bus_address; // for master mode
-
-	boolean do_rep_start; //-
+    uint32_t id;
 
     uint8_t *tx_buffer;
     uint8_t *rx_buffer;
-
     size_t size;
 
     uint32_t errors;
@@ -40,7 +43,7 @@ status i2c_reinit(i2c_inition *p_init);
 status i2c_release(i2c_handler *p_handler); //todo
 status i2c_write(i2c_handler *p_handler);
 status i2c_read(i2c_handler *p_handler);
-boolean i2c_get_ready(i2c_handler *p_handler, boolean is_tx_complete);
+boolean i2c_get_ready(i2c_handler *p_handler, i2c_ready_flags ready_flag);
 status i2c_reset(i2c_handler *p_handler); //todo
 
 //sleep/ wake up
